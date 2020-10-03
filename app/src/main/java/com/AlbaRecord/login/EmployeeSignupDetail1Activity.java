8abfa7fb8ecba4abdfc37d4fa706b25015914963
jaeeun <mibi2325@likelion.org>
2020-10-04 01:15:10 +0900

    SearchEmployee생성

---
+++ app/src/main/java/com/AlbaRecord/login/EmployeeSignupDetail1Activity.java
@@ -0,0 +1,16 @@
+package com.AlbaRecord.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.AlbaRecord.R;
+
+public class EmployeeSignupDetail1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_employee_signup_detail1);
+    }
+}
