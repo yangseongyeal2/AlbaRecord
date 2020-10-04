8d2705a84434cdda903fb185dd0bbe7188413a49
jaeeun <mibi2325@likelion.org>
2020-10-04 16:06:17 +0900

    알바설명화면 생성

---
+++ app/src/main/java/com/AlbaRecord/searchemployee/SearchEmployeeActivity.java
@@ -0,0 +1,16 @@
+package com.AlbaRecord.searchemployee;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.AlbaRecord.R;
+
+public class SearchEmployeeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_employee);
+    }
+}
--- app/src/main/java/com/AlbaRecord/searchemployee/ShowEmployeeActivity.java
+++ app/src/main/java/com/AlbaRecord/searchemployee/ShowEmployeeActivity.java
@@ -1,16 +1,15 @@
-package com.AlbaRecord.EmployeeSearch;
+package com.AlbaRecord.searchemployee;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 import com.AlbaRecord.R;
 
-public class SearchEmployeeActivity extends AppCompatActivity {
+public class ShowEmployeeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search_employee);
+        setContentView(R.layout.activity_show_employee);
     }
 }
