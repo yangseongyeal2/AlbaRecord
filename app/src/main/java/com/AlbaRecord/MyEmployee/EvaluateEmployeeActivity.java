d9f85b393045749ed2e0f9fc3656e7e407c062f7
jaeeun <mibi2325@likelion.org>
2020-10-06 21:16:08 +0900

    evaluate_employee XML추가

---
+++ app/src/main/java/com/AlbaRecord/MyEmployee/EvaluateEmployeeActivity.java
@@ -0,0 +1,16 @@
+package com.AlbaRecord.MyEmployee;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.AlbaRecord.R;
+
+public class EvaluateEmployeeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_evaluate_employee);
+    }
+}
