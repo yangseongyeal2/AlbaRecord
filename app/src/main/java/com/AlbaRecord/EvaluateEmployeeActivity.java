0b2bd0bcd264e46a76ead9ea252fd986a7a3ad27
yangseongyeal2 <61301184+yangseongyeal@users.noreply.github.com>
2020-10-06 22:27:50 +0900

    재은이꺼 합침

---
+++ app/src/main/java/com/AlbaRecord/EvaluateEmployeeActivity.java
@@ -0,0 +1,14 @@
+package com.AlbaRecord;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EvaluateEmployeeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_evaluate_employee);
+    }
+}
