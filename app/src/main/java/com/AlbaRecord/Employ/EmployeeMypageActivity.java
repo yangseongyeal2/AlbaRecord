8ed4143a21f275d8710090f2c21d9ba282b35564
Eunhye <silversun@likelion.org>
2020-10-07 16:10:29 +0900

    은혜언니가 처음한거 !!

---
+++ app/src/main/java/com/AlbaRecord/Employ/EmployeeMypageActivity.java
@@ -0,0 +1,16 @@
+package com.AlbaRecord.Employ;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.AlbaRecord.R;
+
+public class EmployeeMypageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_employee_mypage);
+    }
+}
