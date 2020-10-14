367cef7423360e72f8b7b8f8e01559420f1e22e6
jaeeun <mibi2325@likelion.org>
2020-10-14 14:17:53 +0900

    오래쉴동안했던것들 push

---
+++ app/src/main/java/com/AlbaRecord/Employ/SearchBossActivity.java
@@ -0,0 +1,16 @@
+package com.AlbaRecord.Employ;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.AlbaRecord.R;
+
+public class SearchBossActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_boss);
+    }
+}
---
+++ app/src/main/java/com/AlbaRecord/Employ/ShowBossActivity.java
@@ -0,0 +1,16 @@
+package com.AlbaRecord.Employ;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.AlbaRecord.R;
+
+public class ShowBossActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_boss);
+    }
+}
