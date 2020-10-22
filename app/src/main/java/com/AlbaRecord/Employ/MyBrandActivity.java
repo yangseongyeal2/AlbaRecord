89f1d2ae095755ff629e63f3f94aa59be477500b
jaeeun <mibi2325@likelion.org>
2020-10-22 17:51:36 +0900

    mybrand

---
+++ app/src/main/java/com/AlbaRecord/Employ/MyBrandActivity.java
@@ -0,0 +1,16 @@
+package com.AlbaRecord.Employ;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.AlbaRecord.R;
+
+public class MyBrandActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_brand);
+    }
+}
